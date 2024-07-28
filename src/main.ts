cbc468f7aef091d47a9d748ab3c79ca952521183
aitmamatovmusa <aitmamatovmusa@gmail.com>
2024-07-28 22:40:17 +0500

    Create interceptor to map base data

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,8 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   await app.listen(3000);
 }
