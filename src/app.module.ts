518940184f14b5dc5b2008b1f195dba30409fd97
aitmamatovmusa <aitmamatovmusa@gmail.com>
2024-08-03 11:58:37 +0500

    Deleted unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './features/auth/auth.module';
@@ -18,8 +16,8 @@ import { AppConfigModule } from './database/appConfig/appConfig.module';
     DatabaseModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule implements NestModule {
   constructor(private readonly configService: ConfigService) {}
