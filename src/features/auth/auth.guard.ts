c7a00bc902b215395ef2195b7eb4fb951d1769f2
aitmamatovmusa <aitmamatovmusa@gmail.com>
2024-08-05 01:21:30 +0500

    Exclude some endpoins from auth guard

--- src/features/auth/auth.guard.ts
+++ src/features/auth/auth.guard.ts
@@ -5,13 +5,27 @@ import {
   ExecutionContext,
   Inject,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Cache } from 'cache-manager';
+import { IS_PUBLIC_KEY } from './auth.decorator';
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+  constructor(
+    private reflector: Reflector,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const sessionId = `sess:${request.session.id}`;
     const cacheSessionId = await this.cacheManager.get(sessionId);
     return cacheSessionId ? true : false;
