d45411d9825976a51130a335e99ebd4bcdf0788b
aitmamatovmusa <aitmamatovmusa@gmail.com>
2024-08-06 23:50:59 +0500

    Connect author and quote entities

--- src/features/author/author.entity.ts
+++ src/features/author/author.entity.ts
@@ -1,8 +1,12 @@
 import { BaseEntity } from 'src/database/base.entity';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
+import { Quote } from '../quote/quote.entity';
 
 @Entity()
 export class Author extends BaseEntity {
   @Column()
   fullname: string;
+
+  @OneToMany(() => Quote, (quote) => quote.author)
+  quotes: Quote[];
 }
--- src/features/quote/quote.module.ts
+++ src/features/quote/quote.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { QuoteController } from './quote.controller';
 import { QuoteService } from './quote.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Quote } from './quote.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Quote])],
   controllers: [QuoteController],
-  providers: [QuoteService]
+  providers: [QuoteService],
 })
 export class QuoteModule {}
